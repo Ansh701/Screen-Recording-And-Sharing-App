5ba14225c4321a8f0f708016cd80e9254c6f0404
Ansh701 <151486704+Ansh701@users.noreply.github.com>
2025-08-08 19:00:49 +0530

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                port: "",
+                pathname: "/a/**",
+            },
+        ],
+    },
+};
+
+export default nextConfig;
